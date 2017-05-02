cdbc9d131784a267ff9d05b03dc4ba59e62c43ef
Aditya Shastri <ad.shastri1@gmail.com>
2017-05-02 15:38:28 -0400

    home page

---
+++ public/js/scripts.js
@@ -0,0 +1,19 @@
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip();
+});
+
+function openLinkedin() {
+  window.open('https://linkedin.com/in/aditya-shastri', '_blank');
+}
+
+function openGithub() {
+  window.open('https://github.com/adshastri', '_blank');
+}
+
+function openDevpost() {
+  window.open('https://devpost.com/adityashastri', '_blank');
+}
+
+function openResume() {
+  window.open('public/resume.pdf', '_blank');
+}
